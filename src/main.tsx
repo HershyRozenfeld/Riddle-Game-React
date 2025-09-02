8f6db102c80c621e5d3e4e9326a0c6b9302ed0fe
hershy rozenfeld <Y6764057@gmail.com>
2025-09-02 16:52:37 +0300

    ✨ feat(router): add routes for leaderboard and menu
    - add <LeaderBoard /> page route
    - add <Menu /> page route

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Login from './views/Login.tsx'
 import Play from './views/Play.tsx'
+import LeaderBoard from './views/LeaderBoard.tsx'
+import Menu from './views/Menu.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,6 +15,8 @@ createRoot(document.getElementById('root')!).render(
         <Route path='/' element={<App />} />
         <Route path='/login' element={<Login />} />
         <Route path='/play' element={<Play />} />
+        <Route path='/leaderboard' element={<LeaderBoard />} />
+        <Route path='/menu' element={<Menu />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,

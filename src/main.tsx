cf25cba70f082a768a8523380eee8bb46b0a65cf
hershy rozenfeld <Y6764057@gmail.com>
2025-09-03 17:58:14 +0300

    ✨ feat(main): migrate to react-router-dom v6
    - migrate from react-router v5 to v6 to use new features
    - remove individual route imports and define routes in App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import Login from './views/Login.tsx'
-import Play from './views/Play.tsx'
-import LeaderBoard from './views/LeaderBoard.tsx'
-import Menu from './views/Menu.tsx'
-import Riddle from './views/Riddle.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/play' element={<Play />} />
-        <Route path='/leaderboard' element={<LeaderBoard />} />
-        <Route path='/menu' element={<Menu />} />
-        <Route path='/riddle' element={<Riddle name="" level="" question="" />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
